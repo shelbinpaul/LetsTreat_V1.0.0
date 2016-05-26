3483a18e9d14fa1fc18732e6961fb4f5a0463c29
shelbinpaul <shelbinpaul@gmail.com>
2016-05-26 19:22:44 +0530

    testing commit from studio

--- app/src/main/java/com/example/sandeep/letstreat_v100/HomeScreen.java
+++ app/src/main/java/com/example/sandeep/letstreat_v100/HomeScreen.java
@@ -1,11 +1,14 @@
 package com.example.sandeep.letstreat_v100;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListAdapter;
 
@@ -13,6 +16,7 @@ import com.example.sandeep.letstreat_v100.Adapters.CardAdapter;
 
 public class HomeScreen extends AppCompatActivity {
 
+    EditText mEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +41,20 @@ public class HomeScreen extends AppCompatActivity {
         LinearLayoutManager llm_Hospital = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         rv_Hospitals.setLayoutManager(llm_Hospital);
 
-    //here testing git
-        //second testing
-        //third testing
+        mEditText = (EditText)findViewById(R.id.editText1);
+        mEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreen.this,SearchList_Activity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_in);
+
+
+            }
+        });
+
+
+
     }
 
     @Override
